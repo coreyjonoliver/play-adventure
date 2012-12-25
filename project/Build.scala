838daa558c22218422660cee29ee491437a1c8bf
Corey Oliver <corey.jon.oliver@gmail.com>
2012-12-25 12:04:41 -0600

    initial commit

---
+++ project/Build.scala
@@ -0,0 +1,28 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+import org.ensime.sbt.Plugin.Settings.ensimeConfig
+import org.ensime.sbt.util.SExp._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "play-allowance"
+    val appVersion      = "1.0-SNAPSHOT"
+
+    val appDependencies = Seq(
+      // Add your project dependencies here,
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+      // Add your own project settings here      
+      ensimeConfig := sexp(
+        key(":only-include-in-index"), sexp(
+          "controllers\\..*",
+          "models\\..*",
+          "views\\..*",
+          "play\\..*"
+        )
+    )
+}
+
